747e5320c743f0d6666e04217cb91589e3a86a5f
司芳源 <sify1221@gmail.com>
2021-03-04 15:04:21 +0800

    f

---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.ConfigurationBuilderEvent;
+import org.apache.commons.configuration2.builder.ReloadingFileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.configuration2.reloading.PeriodicReloadingTrigger;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static PropertiesConfiguration config;
+
+    public static void main(String[] args) {
+        Parameters params = new Parameters();
+        File propertiesFile = new File(args[0]);
+        ReloadingFileBasedConfigurationBuilder<PropertiesConfiguration> builder = new ReloadingFileBasedConfigurationBuilder<>(PropertiesConfiguration.class)
+                .configure(params.fileBased().setFile(propertiesFile));
+        builder.addEventListener(ConfigurationBuilderEvent.ANY, event -> {
+            if (event.getEventType() == ConfigurationBuilderEvent.RESET) {
+                try {
+                    config = builder.getConfiguration();
+                } catch (ConfigurationException e) {
+                    System.out.println("重新加载配置文件错误");
+                    e.printStackTrace();
+                }
+            }
+        });
+        PeriodicReloadingTrigger periodicReloadingTrigger = new PeriodicReloadingTrigger(builder.getReloadingController(), null, 15,
+                TimeUnit.SECONDS);
+        periodicReloadingTrigger.start();
+        try {
+            config = builder.getConfiguration();
+        } catch (ConfigurationException e) {
+            System.out.println("初始化加载配置文件错误");
+            e.printStackTrace();
+        }
+        config.getKeys().forEachRemaining(System.out::println);
+    }
+}
